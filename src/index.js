acf4975f9bf389900bb3e5952e6b4862df8675a4
Renee Heyward <info@developerzblock.com>
2021-02-22 23:13:12 -0500

    Finally Setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1 className="text-red-500">Hello, World</h1>,
+  <h1 className="text-blue-500">Yoooo, World</h1>,
   document.getElementById('root')
 );
