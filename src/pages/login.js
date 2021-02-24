cca58d78d787e210bc38650dcd7ffa11bbf7abcd
Renee Heyward <info@developerzblock.com>
2021-02-24 11:00:25 -0500

    Moving Images Folder

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,7 +17,7 @@ export default function Login() {
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       
       <div className="flex w-3/5 px-6">
-        <img src={"/images/iphone-with-profile.jpg"} alt="iPhone with Instagram app" />
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
       </div>
       
       <div className="flex flex-col w-2/5">
